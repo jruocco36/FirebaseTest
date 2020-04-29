7fe6c6fa948fbb1ade950aad48d2c28e048435a4
Jeff R <jruocco36@gmail.com>
2020-04-29 17:35:33 -0400

    added sign out button

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,15 +1,15 @@
-import 'package:firebase_test/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import 'authenticate/authenticate.dart';
+import 'package:firebase_test/models/user.dart';
+import 'package:firebase_test/screens/authenticate/authenticate.dart';
+import 'package:firebase_test/screens/home/home.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
     print(user);
-    
-    return Authenticate();
+
+    return user == null ? Authenticate() : Home();
   }
 }
